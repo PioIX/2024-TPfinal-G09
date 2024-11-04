913419007325b918a8fec562fc0fae0ffe009276
EzeGreco <egreco@pioix.edu.ar>
2024-11-04 10:06:23 -0300

    FALTA
    hay que hacer endGame

--- frontend/src/app/endGame/page.js
+++ frontend/src/app/endGame/page.js
@@ -1,52 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import './EndGamePage.css'; // Asegúrate de tener estilos específicos para la página
+"use client"
+import Button from "@/components/button";
+import React, { useState, useEffect } from "react";
+import styles from "@/app/page.module.css"; // Estilos para el formulario
+import { useSearchParams } from 'next/navigation';
 
-const EndGamePage = () => {
-  const [gameData, setGameData] = useState(null);
-  const [isWinner, setIsWinner] = useState(false);
-
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const idJuego = params.get('idJuego');
-  const idUser = params.get('idUser');
-
-  useEffect(() => {
-    const fetchGameData = async () => {
-      try {
-        const response = await fetch(`/api/games/${idJuego}`);
-        const game = await response.json();
-
-        setGameData(game);
-        setIsWinner(game.winner === idUser);
-      } catch (error) {
-        console.error("Error al obtener los datos del juego:", error);
-      }
-    };
-
-    fetchGameData();
-  }, [idJuego, idUser]);
-
-  if (!gameData) {
-    return <div>Cargando...</div>;
+export default function Home() {
+  
+  const searchParams = useSearchParams();
+  const idUser = searchParams.get('idUser');
+  const idJuego = searchParams.get('idJuego');
+  async function goRecord(){
+    window.location.href = `/historial?idUser=${userId}`
   }
+  
 
   return (
-    <div className={`end-game-page ${isWinner ? 'winner' : 'loser'}`}>
-      <h1>{isWinner ? '¡Felicidades, ganaste!' : 'Perdiste esta vez, ¡sigue intentando!'}</h1>
-      
-      <div className="game-info">
-        <p><strong>Fecha y hora de la partida:</strong> {new Date(gameData.date).toLocaleString()}</p>
-        <p><strong>Ganador:</strong> {gameData.winnerUser}</p>
-        <h2>Otros jugadores:</h2>
-        <ul>
-          {gameData.players.map(player => (
-            <li key={player.id}>{player.username}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
+      <main>
+        <div className={styles.container}>
+        <h2>Bienvenido a PioCards</h2>
+          <Button onClick={goPlay}>Jugar</Button>
+          <Button onClick={goMazo}>Mazo</Button>
+          <Button onClick={goRecord}>Historial</Button>
+          <Button onClick={goUsers}>Perfil</Button>        
+          <Button onClick={goTienda}>Tienda</Button>        
 
-export default EndGamePage;
+<h2>REGLAS DEL JUEGO</h2>
+
+          <h3>
+            En este juego competiras con tus 5 cartas contra las 5 cartas del jugador de la otra maquina
+          </h3>
+          <ol style={{marginLeft : "100px"}}>
+              <li>Leer todas las consignas</li>
+              <li>Elegir las 5 cartas para jugar dentro de la pestaña mazo</li>
+              <li>Ir a la pestaña Jugar y esperar a que empiece el juego</li>
+          </ol>
+          <h3> Una vez en el juego, podrás ver tus cartas junto con un mensaje que indicará si eres tú quien debe elegir la característica o no.</h3>
+          <ol>
+            <li> elegir la caracterista con la que deseas empezar la primera ronda // esperar a que tu rival eliga</li>
+            <li> elegir una carta para jugar esa ronda</li>
+          </ol>
+
+        </div>
+      </main>
+  );
+}
+
