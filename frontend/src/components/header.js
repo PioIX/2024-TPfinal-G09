b86ac768bb60042750aa91b670544e730b41865a
Marturelo007 <163536642+Marturelo007@users.noreply.github.com>
2024-11-18 14:23:31 -0300

    error mostrar dinero solucionado

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,12 +1,28 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './header.module.css';
+import { getUserById } from "@/functions/fetch.js";
 import ActualProfilePic from './actualProfilePic';
 
 export default function Header({ username, profileImage, idUser, money }) {
+  const [userData, setUserData] = useState(null);
+
+  // Función para obtener los datos del usuario
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const data = await getUserById(idUser);
+      setUserData(data); // Guardamos los datos del usuario
+    };
+
+    fetchUserData();
+  }, [idUser]);
+
   async function menu() {
     window.location.href = `/menu?idUser=${idUser}`;
   }
+
+  // Condicional para mostrar el dinero solo si userData es un objeto válido
+  const shouldShowMoney = userData !== null && userData !== undefined;
 
   return (
     <header className={styles.header}>
@@ -17,11 +33,13 @@ export default function Header({ username, profileImage, idUser, money }) {
         </h1>
       </div>
 
-      {/* Mostrar el dinero del usuario */}
-      <div className={styles.money}>
-        <span className={styles.username}>Dinero: </span>
-        <span className={styles.logoSecondary}>${money}</span>
-      </div>
+      {/* Solo mostrar el div de dinero si userData es un objeto válido */}
+      {shouldShowMoney && (
+        <div className={styles.money}>
+          <span className={styles.username}>Dinero: </span>
+          <span className={styles.logoSecondary}>${money}</span>
+        </div>
+      )}
 
       <div className={styles.userInfo}>
         <span className={styles.username}>{username}</span>
@@ -30,4 +48,3 @@ export default function Header({ username, profileImage, idUser, money }) {
     </header>
   );
 }
-
